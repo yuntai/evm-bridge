63efe5e69c416e668d4eeb9845952fe695c15777
yuntai <yuntai.kyong@gmail.com>
2022-02-12 07:01:17 +0900

    Bug fix in contract and add more single-sided test
    - add counter to generate unique hash id
    - add require() test msg.sender agains record
      inside external funcs containing transfer tokens
    - format fix - no tab, 2 indent space
    - add test sender side's revert case

--- contracts/bridge.sol
+++ contracts/bridge.sol
@@ -2,157 +2,164 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {
+  SafeERC20,
+  IERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 //TODO: use openzepplin
 contract Ownable {
-    address public owner;
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner");
-        _;
-    }
 
-    function changeOwner(address newOwner) external onlyOwner {
-        require(newOwner != address(0), "zero address");
-        owner = newOwner;
-    }
+  address public owner;
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only owner");
+    _;
+  }
+
+  function changeOwner(address newOwner) external onlyOwner {
+    require(newOwner != address(0), "zero address");
+    owner = newOwner;
+  }
 }
 
 contract Bridge is Ownable {
-    using SafeERC20 for ERC20;
-
-	constructor(address _relay, address _token) {
-		owner = msg.sender;
-		relay = _relay;
-		token = _token;
-	}
+  constructor(address _relay, address _token) {
+    owner = msg.sender;
+    relay = _relay;
+    token = _token;
+  }
 
     //TODO: use openzepplin role library
-	modifier onlyRelay {
-		require(msg.sender == relay);
-		_;
-	}	
+  modifier onlyRelay {
+    require(msg.sender == relay);
+    _;
+  }
 
-	enum TransferState {
-		LOCKED,
-		REVERT_REQUESTED,
-		REVERTED,
-		REDEEMED,
-        RELEASED
-	}
+  enum TransferState {
+    LOCKED,
+    REVERT_REQUESTED,
+    REVERTED,
+    REDEEMED,
+    RELEASED
+  }
 
-	struct TransferRecord {
-		bytes32 id;
-		uint from_chain_id; 
-		uint to_chain_id; 
-		address from_address; 
-		address to_address; 
-		address from_token; 
-		address to_token;
-		uint amount;
-		TransferState state;
-	}
+  struct TransferRecord {
+    bytes32 id;
+    uint from_chain_id;
+    uint to_chain_id;
+    address from_address;
+    address to_address;
+    address from_token;
+    address to_token;
+    uint amount;
+    TransferState state;
+  }
 
-	event LockEvent(bytes32 indexed);
-	event RevertRequestEvent(bytes32 indexed);
-	event RevertResponseEvent(bytes32 indexed, TransferState);
+  event LockEvent(bytes32 indexed);
+  event RevertRequestEvent(bytes32 indexed);
+  event RevertResponseEvent(bytes32 indexed, TransferState);
 
-	address relay;
-    address token;
+  address relay;
+  address token;
+  uint counter;
 
-	// assumption under having a single peer chain and single type of token
-	// later map(chain -> map(token -> peer_balance) 
-	uint peer_balance;
+  // assumption under having a single peer chain and single type of token
+  // later map(chain -> map(token -> peer_balance) 
+  uint public peer_balance;
 
-	// transfer record
-	mapping(bytes32=>TransferRecord) public records;
+  // transfer record
+  mapping(bytes32=>TransferRecord) public records;
 
-	function _generateId(uint from_chain_id, uint to_chain_id, address from_address, address to_address, address from_token, address to_token, uint amount) internal pure returns(bytes32) {
-		bytes32 digest = keccak256(                                                                     
-			abi.encodePacked(
-                from_chain_id, to_chain_id,
-				from_address, to_address,
-				from_token, to_token,
-				amount
-			)                                                                                           
-		);                                                                                              
-		return digest;
-	}
+  function _generateId(uint from_chain_id, uint to_chain_id, address from_address, address to_address, address from_token, address to_token, uint amount, uint _counter) internal pure returns(bytes32) {
+    bytes32 digest = keccak256(
+      abi.encodePacked(
+        from_chain_id, to_chain_id,
+        from_address, to_address,
+        from_token, to_token,
+        amount,
+        _counter
+      )
+    );
+    return digest;
+  }
 
-	function lock(uint to_chain_id, address to_address, address from_token, address to_token, uint amount) external {
-		require(peer_balance > amount, "no sufficient peer balance");
-        bytes32 id = _generateId(block.chainid, to_chain_id, msg.sender, to_address, from_token, to_token, amount);
-		TransferRecord memory rec = TransferRecord({
-			id: id,
-			from_chain_id: block.chainid,
-			to_chain_id: to_chain_id,
-			from_address: msg.sender,
-			to_address: to_address,	
-			from_token: from_token,
-			to_token: to_token,
-            amount: amount,
-			state: TransferState.LOCKED
-		});
+  function lock(uint to_chain_id, address to_address, address from_token, address to_token, uint amount) external {
+    require(peer_balance > amount, "no sufficient peer balance");
 
-		peer_balance -= amount;
-		records[id] = rec;	
-		ERC20(token).safeTransferFrom(msg.sender, address(this), amount);
-		emit LockEvent(id);
+    bytes32 id = _generateId(block.chainid, to_chain_id, msg.sender, to_address, from_token, to_token, amount, counter);
+    TransferRecord memory rec = TransferRecord({
+      id: id,
+      from_chain_id: block.chainid,
+      to_chain_id: to_chain_id,
+      from_address: msg.sender,
+      to_address: to_address,
+      from_token: from_token,
+      to_token: to_token,
+      amount: amount,
+      state: TransferState.LOCKED
+    });
+
+    peer_balance -= amount;
+    records[id] = rec;
+    counter += 1;
+    SafeERC20.safeTransferFrom(IERC20(token), msg.sender, address(this), amount);
+    emit LockEvent(id);
     }
 
-	function redeem(bytes32 id) external {
-		require(records[id].id != 0x0, "record not found");
-		require(records[id].state == TransferState.REVERTED, "invalid state");
-		records[id].state = TransferState.REDEEMED;
-		ERC20(token).safeTransferFrom(address(this), records[id].from_address, records[id].amount);
+  function redeem(bytes32 id) external {
+    require(records[id].id != 0x0, "record not found");
+    require(records[id].state == TransferState.REVERTED, "invalid state");
+    require(msg.sender == records[id].from_address, "invalid user");
+    records[id].state = TransferState.REDEEMED;
+    SafeERC20.safeTransfer(IERC20(token), records[id].from_address, records[id].amount);
     }
 
-	function revert_request(bytes32 id) external {
-		require(records[id].id != 0x0, "record not found");
-		require(records[id].state == TransferState.LOCKED);
-	  	records[id].state = TransferState.REVERT_REQUESTED;
-		emit RevertRequestEvent(id);
+  function revert_request(bytes32 id) external {
+    require(records[id].id != 0x0, "record not found");
+    require(records[id].state == TransferState.LOCKED);
+    records[id].state = TransferState.REVERT_REQUESTED;
+    emit RevertRequestEvent(id);
+  }
+
+  function handle_revert_response(bytes32 id, TransferState state) external onlyRelay {
+    require(records[id].id != 0x0, "record not found");
+    require(records[id].state == TransferState.REVERT_REQUESTED, "invalid state");
+    records[id].state = state;
+
+    if(state == TransferState.REVERTED)
+      peer_balance += records[id].amount;
+  }
+
+  function handle_lock(TransferRecord calldata record) external onlyRelay {
+    peer_balance += record.amount;
+    records[record.id] = record;
+    records[record.id].state = TransferState.LOCKED;
+  }
+
+  function release(bytes32 id) external {
+    require(records[id].id != 0x0, "record not found");
+    require(records[id].state == TransferState.LOCKED);
+    require(msg.sender == records[id].to_address, "invalid user");
+    records[id].state = TransferState.RELEASED;
+    SafeERC20.safeTransfer(IERC20(token), records[id].to_address, records[id].amount);
+  }
+
+  function handle_revert_request(bytes32 id) external onlyRelay {
+    require(records[id].id != 0x0, "record not found");
+    require(records[id].state == TransferState.LOCKED || records[id].state == TransferState.RELEASED, "invalid state");
+    if(records[id].state == TransferState.LOCKED) {
+      peer_balance -= records[id].amount;
+      records[id].state = TransferState.REVERTED;
     }
+    emit RevertResponseEvent(id, records[id].state);
+  }
 
-	function handle_revert_response(bytes32 id, TransferState state) external onlyRelay {
-		require(records[id].id != 0x0, "record not found");
-		require(records[id].state == TransferState.REVERT_REQUESTED, "invalid state");
-		records[id].state = state;
-
-	  	if(state == TransferState.REVERTED)
-			peer_balance += records[id].amount;
-    }
-
-	function handle_lock(TransferRecord calldata record) external onlyRelay {
-		peer_balance += record.amount;
-		records[record.id] = record;
-		records[record.id].state = TransferState.LOCKED;
-	}
-			
-	function release(bytes32 id) external {
-		require(records[id].id != 0x0, "record not found");
-  		require(records[id].state == TransferState.LOCKED);
-  		records[id].state = TransferState.RELEASED;
-  		ERC20(token).safeTransferFrom(address(this), msg.sender, records[id].amount);
-    }
-
-
-	function handle_revert_request(bytes32 id) external onlyRelay {
-		require(records[id].id != 0x0, "record not found");
-  		require(records[id].state == TransferState.LOCKED || records[id].state == TransferState.RELEASED, "invalid state");
-		if(records[id].state == TransferState.LOCKED) {
-			peer_balance -= records[id].amount;
-    		records[id].state = TransferState.REVERTED;
-		}
-		emit RevertResponseEvent(id, records[id].state);
-    }
-
-	function approve() external {} // approve bridge to spend msg.sender's token
-	function deposit() external onlyOwner {} // deposit token from owner
-	function withdrawal(address) external onlyOwner {} // withrawal from owner
-	function setRelay(address) external onlyOwner {} // set relay identity
-	function setPeerBalance(uint amount) external onlyOwner { // manual balance
-        peer_balance = amount;
-    }
+  function approve() external {} // approve bridge to spend msg.sender's token
+  function deposit() external onlyOwner {} // deposit token from owner
+  function withdrawal(address) external onlyOwner {} // withrawal from owner
+  function setRelay(address) external onlyOwner {} // set relay identity
+  function setPeerBalance(uint amount) external onlyOwner { // manual balance
+    peer_balance = amount;
+  }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -20,6 +20,10 @@ describe("Bridge", function () {
     token.connect(bob).approve(bridge.address, 10000);
 
     await bridge.setPeerBalance(100000)
+    initial_peer_balance = await bridge.peer_balance();
+
+    bridgeBegBal = await token.balanceOf(bridge.address);
+    bobBegBal = await token.balanceOf(bob.address);
 
     to_chain_id = 7777;
     from_chain_id = 31337; // hardhat default chain id TODO: how to access network config here
@@ -32,24 +36,25 @@ describe("Bridge", function () {
   });
 
   it("Sender Lock", async function () {
-    const amt = 1000;
     const expectedId = soliditySha3(
       from_chain_id, to_chain_id,
       bob.address, alice.address,
       token.address, token.address,
-      amt
+      1000,
+      0
     );
-    const begBal = await token.balanceOf(bridge.address);
     const tx = await bridge.connect(bob).lock(to_chain_id, alice.address, token.address, token.address, 1000);
     await expect(tx).to.emit(bridge, 'LockEvent').withArgs(expectedId);
     await expect(bridge.records(expectedId).state == LOCKED);
+    expect(await bridge.peer_balance()).to.equal(initial_peer_balance - 1000);
     //const receipt = await tx.wait();
     // approve/transfer events from token
     //for (const event of receipt.events) {
     //  console.log(event);
     //  console.log(`Event ${event.event} with args ${event.args}`);
     // }
-    await expect(await token.balanceOf(bridge.address)).to.equal(begBal + 1000);
+    expect(await token.balanceOf(bob.address)).to.equal(bobBegBal - 1000);
+    expect(await token.balanceOf(bridge.address)).to.equal(bridgeBegBal + 1000);
   });
 
   it("Sender Lock & Revert", async function () {
@@ -57,16 +62,42 @@ describe("Bridge", function () {
       from_chain_id, to_chain_id,
       bob.address, alice.address,
       token.address, token.address,
-      1001
+      1001,
+      0
     );
+    const initial_peer_balance = await bridge.peer_balance();
     await expect(await bridge.connect(bob).lock(to_chain_id, alice.address, token.address, token.address, 1001)).to.emit(bridge, 'LockEvent').withArgs(_id);
     await expect(bridge.connect(bob).revert_request(_id)).to.emit(bridge, 'RevertRequestEvent').withArgs(_id);
     await expect(bridge.records(_id).state == REVERT_REQUESTED);
 
     // event push
-    bridge.connect(relay).handle_revert_response(_id, REVERTED);
+    await bridge.connect(relay).handle_revert_response(_id, REVERTED);
     await expect(bridge.records(_id).state == REVERTED);
-    //TODO: peer balance check
-    //await expect(.records(_id).state == REVERTED);
+    // peer balance restored
+    expect(await bridge.peer_balance()).to.equal(initial_peer_balance);
+  });
+
+  it("Sender Lock & Revert & Redeem", async function () {
+    const _id = soliditySha3(
+      from_chain_id, to_chain_id,
+      bob.address, alice.address,
+      token.address, token.address,
+      1002,
+      0
+    );
+
+    const initial_peer_balance = await bridge.peer_balance();
+    await bridge.connect(bob).lock(to_chain_id, alice.address, token.address, token.address, 1002)
+    await bridge.connect(bob).revert_request(_id);
+
+    // event push
+    await bridge.connect(relay).handle_revert_response(_id, REVERTED);
+
+    //await bridge.redeem(_id); TODO: raise
+    await bridge.connect(bob).redeem(_id);
+    await expect(bridge.records(_id).state == REDEEMED);
+
+    expect(await token.balanceOf(bridge.address)).to.equal(bridgeBegBal);
+    expect(await token.balanceOf(bob.address)).to.equal(bobBegBal);
   });
 });
