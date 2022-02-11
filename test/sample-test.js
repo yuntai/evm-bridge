160277423c4e9ede777f0b46844bcc4afccb3d15
yuntai <yuntai.kyong@gmail.com>
2022-02-11 21:44:48 +0900

    Added more single-sided test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { soliditySha3 } = require("web3-utils");
 
 describe("Bridge", function () {
   beforeEach(async function() {
@@ -16,18 +17,56 @@ describe("Bridge", function () {
     token.approve(bob.address, 10000);
     token.transfer(bob.address, 10000);
 
-    token.connect(bob).approve(bridge.address, 1000);
+    token.connect(bob).approve(bridge.address, 10000);
 
     await bridge.setPeerBalance(100000)
 
     to_chain_id = 7777;
+    from_chain_id = 31337; // hardhat default chain id TODO: how to access network config here
+
+    LOCKED = 0;
+    REVERT_REQUESTED = 1;
+    REVERTED = 2;
+    REDEEMED = 3;
+    RELEASED = 4;
   });
 
-  it("Lock and release single side", async function () {
+  it("Sender Lock", async function () {
     const amt = 1000;
+    const expectedId = soliditySha3(
+      from_chain_id, to_chain_id,
+      bob.address, alice.address,
+      token.address, token.address,
+      amt
+    );
     const begBal = await token.balanceOf(bridge.address);
-    await expect(bridge.connect(bob).lock(to_chain_id, alice.address, token.address, token.address, amt)).to.emit(bridge, 'LockEvent');
-    await expect(await token.balanceOf(bridge.address)).to.equal(begBal + amt);
+    const tx = await bridge.connect(bob).lock(to_chain_id, alice.address, token.address, token.address, 1000);
+    await expect(tx).to.emit(bridge, 'LockEvent').withArgs(expectedId);
+    await expect(bridge.records(expectedId).state == LOCKED);
+    //const receipt = await tx.wait();
+    // approve/transfer events from token
+    //for (const event of receipt.events) {
+    //  console.log(event);
+    //  console.log(`Event ${event.event} with args ${event.args}`);
+    // }
+    await expect(await token.balanceOf(bridge.address)).to.equal(begBal + 1000);
+  });
 
+  it("Sender Lock & Revert", async function () {
+    const _id = soliditySha3(
+      from_chain_id, to_chain_id,
+      bob.address, alice.address,
+      token.address, token.address,
+      1001
+    );
+    await expect(await bridge.connect(bob).lock(to_chain_id, alice.address, token.address, token.address, 1001)).to.emit(bridge, 'LockEvent').withArgs(_id);
+    await expect(bridge.connect(bob).revert_request(_id)).to.emit(bridge, 'RevertRequestEvent').withArgs(_id);
+    await expect(bridge.records(_id).state == REVERT_REQUESTED);
+
+    // event push
+    bridge.connect(relay).handle_revert_response(_id, REVERTED);
+    await expect(bridge.records(_id).state == REVERTED);
+    //TODO: peer balance check
+    //await expect(.records(_id).state == REVERTED);
   });
 });
