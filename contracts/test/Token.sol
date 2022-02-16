2e8676b6b7c7f1ae2e9e3b300266431a10a15db1
yuntai <yuntai.kyong@gmail.com>
2022-02-16 11:56:11 +0900

    Minor changes in test Token contract

--- contracts/test/Token.sol
+++ contracts/test/Token.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    constructor() ERC20("Token", "TOK") {
         // Mint 100 tokens to msg.sender
         // Similar to how
         // 1 dollar = 100 cents
         // 1 token = 1 * (10 ** decimals)
-        _mint(msg.sender, 100 * 10**uint(decimals()));
+        _mint(msg.sender, 1000 * 10**uint(decimals()));
     }
 }
