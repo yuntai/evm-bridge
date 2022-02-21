7791b71bac4deeaa4f687830dbfe97d5ebb523c1
yuntai <yuntai.kyong@gmail.com>
2022-02-21 15:03:33 +0900

    make easy to make .env file

--- scripts/gen-address.js
+++ scripts/gen-address.js
@@ -1,14 +1,14 @@
-// genearted with
 const ethWallet = require('ethereumjs-wallet');
 
 pvks = []
-pubks = []
 
-for(let index=0; index < 6; index++) {
-  let addressData = ethWallet.generate();
+for(let index=0; index<8; index++) {
+  let addressData = ethWallet.default.generate();
   pvks.push(addressData.getPrivateKeyString());
-  pubks.push(addressData.getAddressString());
+  //pubks.push(addressData.getAddressString());
 }
 
-console.log(pvks);
-console.log(pubks);
+x=`ACCOUNTS1=${JSON.stringify(pvks.slice(0,4))}
+ACCOUNTS2=${JSON.stringify(pvks.slice(4))}`
+console.log(x);
+
