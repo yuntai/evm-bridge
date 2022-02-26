56b4c28924e501a045bdab373741a0da86d35ddd
yuntai <yuntai.kyong@gmail.com>
2022-02-26 14:26:19 +0900

    fix db update

--- tasks/db.ts
+++ tasks/db.ts
@@ -1,11 +1,11 @@
 // Importing SQLite3 to our project.
 var sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("db.db");
 
 export async function addRecord(
 	id: string,
 	fromchain: string, tochain: string,
 	from: string, to: string, amount: number) {
+    const db = new sqlite3.Database("db.db");
 	return new Promise((resolve, reject) => {
 		db.run("INSERT INTO records VALUES ($id, $fromchain, $tochain, $from, $to, $amount, $state)", {
 			$id: id,
@@ -17,34 +17,41 @@ export async function addRecord(
 			$state: 'LOCKED'
 		}, function (err: any) {
 			if (err) reject(err);
-			else resolve(true);
+			else {
+                db.close();
+                resolve(true);
+            }
 		});
 	});
 }
 
 
 export function updateState(id: string, state: string) {
+    const db = new sqlite3.Database("db.db");
+    var inputData = [state, id];
 	return new Promise((resolve, reject) => {
-		db.run("UPDATE records SET state='$state' WHERE id='$id'"), {
-			$id: id,
-			$state: state
-		}, function (err: any) {
-			if (err) reject(err);
-			else {
-				db.run('commit');
-				resolve(true);
-			}
-		};
+        db.run("UPDATE records SET state=? WHERE id=?", inputData, function (err: any, rows: any) {
+            if (err) reject(err);
+            else {
+                console.log(inputData, "ok");
+                db.close();
+                resolve(rows);
+            }
+        });
 	});
 }
 
 export function dumpRecords() {
 	// Add a task to the todo list.
+    const db = new sqlite3.Database("db.db");
 	return new Promise((resolve, reject) => {
 		db.all("SELECT * FROM records",
 			function (err: any, rows: any) {
 				if (err) reject(err);
-				else resolve(rows);
+				else {
+                    db.close();
+                    resolve(rows);
+                }
 			});
 	});
 }
